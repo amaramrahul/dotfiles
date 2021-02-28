1547eaea05ab9f8920e8872b96f282d140224c85
Rahul Amaram <rahulamaram@fb.com>
2021-02-28 15:54:42 +0000

    Fix macos backup.sh and take fresh backup

--- macos/backup.sh
+++ macos/backup.sh
@@ -4,19 +4,19 @@ set -eu
 backup_dir=$(dirname "$0")/backup
 
 if [ -f ~/.bashrc ]; then
-  cp -a ~/.bashrc "$backup_dir"/
+  cp -a ~/.bashrc "$backup_dir"
 fi
 
 if [ -f ~/.config/karabiner/karabiner.json ]; then
-  cp -a ~/.config/karabiner/karabiner.json "$backup_dir"/
+  cp -a ~/.config/karabiner/karabiner.json "$backup_dir"
 fi
 
-if [ -f ~/Library/LaunchAgents/callItADay.plist]; then
-  cp -a ~/Library/LaunchAgents/callItADay.plist "$backup_dir"/
+if [ -f ~/Library/LaunchAgents/callItADay.plist ]; then
+  cp -a ~/Library/LaunchAgents/callItADay.plist "$backup_dir"
 fi
 
-if [ -f ~/Library/LaunchAgents/lockOnInactivity.plist]; then
-  cp -a ~/Library/LaunchAgents/lockOnInactivity.plist "$backup_dir"/
+if [ -f ~/Library/LaunchAgents/lockOnInactivity.plist ]; then
+  cp -a ~/Library/LaunchAgents/lockOnInactivity.plist "$backup_dir"
 fi
 
 crontab -l > "$backup_dir"/crontab
