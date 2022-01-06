dfcea7f0982ad04b8d671a18691794c0fef920ab
Rahul Amaram <rahulamaram@fb.com>
2022-01-06 20:04:15 +0000

    Updated macOS call it a day script

--- macOS/scripts/os-x-call-it-a-day.sh
+++ macOS/scripts/os-x-call-it-a-day.sh
@@ -8,14 +8,14 @@ function lock {
 }
 
 while true; do
-  if test $(date +%H) -eq 22 && test $(date +%M) -eq 55; then
-    osascript -e "display notification \"Lockdown in 5 minutes\" with title \"***** LOCKDOWN REMINDER *****\""
-    sleep 300
+  if test $(date +%H) -eq 21 && test $(date +%M) -eq 45; then
+    osascript -e "display notification \"Lockdown in 15 minutes\" with title \"***** LOCKDOWN REMINDER *****\""
+    sleep 900
     lock
-  elif (test $(date +%H) -ge 22 && test $(date +%M) -ge 30) || test $(date +%H) -lt 5; then
+  elif (test $(date +%H) -ge 22 && test $(date +%M) -ge 00) || test $(date +%H) -lt 5; then
     isSessionLocked=$(python -c 'import sys,Quartz; d=Quartz.CGSessionCopyCurrentDictionary(); print d' | grep "CGSSessionScreenIsLocked = 1" > /dev/null && echo true || echo false)
     if ! $isSessionLocked; then
-      if reason=$(osascript -e "tell application \"System Events\"" -e "activate" -e "choose from list {\"Deep Work (hard-stop at midnight)\", \"Working on an outage\", \"Need to give updates which others are waiting for today\", \"Collaboration with US teams, which if not done, would block progress of critical deliverables\", \"Need to start a background job which would take more than 1 hour\", \"Got a hard-deadline today or tomorrow\", \"Add new exception\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"} with prompt \"Why have you unlocked?\"" -e "end tell"); then
+      if reason=$(osascript -e "tell application \"System Events\"" -e "activate" -e "choose from list {\"Working on an outage\", \"Need to give updates which others are waiting for today\", \"Collaboration with US teams, which if not done, would block progress of critical deliverables\", \"Need to start a background job which would take more than 1 hour\", \"Got a hard-deadline today or tomorrow\", \"Add new exception\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\"} with prompt \"Why have you unlocked?\"" -e "end tell"); then
         if echo $reason | grep "false" >/dev/null; then
           lock
         elif output=$(osascript -e "tell application \"System Events\"" -e "activate" -e "display dialog \"How long to finish the job?\" buttons {\"15 mins\", \"30 mins\", \"1 hr\"} giving up after 10" -e "end tell" 2>/dev/null); then
